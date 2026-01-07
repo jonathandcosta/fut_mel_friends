171c81ec831adb33de3cf50f12f85c1c272194ec
Jonathan Costa <jonathanduartecosta@gmail.com>
2026-01-07 19:02:48 -0300

    chore: delet last project add new project hmlt+tailwin and js

--- lista_pelada/src/components/BotaoAdd/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Circulo } from "./styles"
-
-const BotaoAdd = () => {
-  return (
-    <Circulo to='/cadastrar'>+</Circulo>
-  )
-}
-
-export default BotaoAdd
--- lista_pelada/src/styles/variaveis.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  vermelho: '#c23616',
-  verde: '#44bd32',
-  azul: '#0557cd',
-  amarelo: '#f0c11a',
-  laranja: '#ec6c06',
-  diarista: '#4d9c91',
-  convidado: '#a50cb5',
-};
